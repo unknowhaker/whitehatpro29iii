8f3a4b71557a9333988dfcf709ad425e459d286c
irinsajan <69659076+irinsajan@users.noreply.github.com>
2021-01-06 17:49:27 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -3,7 +3,7 @@ class Box{
         var options = {
           'restitution':0.8,
           'friction':1.0,
-          'density':0.8
+          'density':0.2
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
